1c225f08844676307b3404035a0155ecc14d60ef
Khairul Araf <bsse0631@iit.du.ac.bd>
2016-12-31 18:47:55 +0600

    Initial Commit

---
+++ app/src/main/java/com/androidchatapp/UserDetails.java
@@ -0,0 +1,10 @@
+package com.androidchatapp;
+/**
+ * Created by LINKON on 12/29/2016.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
